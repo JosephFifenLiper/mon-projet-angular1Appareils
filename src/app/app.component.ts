0045b59d4b482f58ce436e6766e2d5d177854415
joseph liper <pascalfifen@gmail.com>
2019-10-17 21:19:39 +0100

    les fichiers ajouter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  autor: string = "Josseph Pascal Liper Fifen Ousseni ";
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) =>{
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 4000
+    );
+  });
+
+  appareils= [
+    {
+      name: 'fien ',
+      status: 'éteint'
+    },
+    {
+      name: 'ousseni ',
+      status: 'allumé'
+    },
+    {
+      name: 'joseph ',
+      status: 'éteint'
+    },
+    {
+      name: 'pascal ',
+      status: 'allumé'
+    }
+  ];
+
+  constructor(){
+    setTimeout(
+      () =>{
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllume(){
+    alert("tout allumer");
+  }
+
 }
